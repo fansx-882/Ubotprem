b98306b1f32d4f4deb71670a23ddcbcd647fe227
fansx-882 <storefansx28@gmail.com>
2025-08-15 23:40:22 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -13,7 +13,7 @@ API_HASH = os.getenv("API_HASH", "77a7094ef80a4296f5d4f54638e833fa")
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8276692772:AAHksYZp8fyls08qztxoNLtHL5R9fokHYZI")
 
-OWNER_ID = int(os.getenv("1714407109", ""))
+OWNER_ID = int(os.getenv("OWNER_ID", "1714407109"))
 
 BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002312835928").split()))
 
