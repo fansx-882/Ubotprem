622dedbeccbe002ff6608d37c60ace57f3f21598
userbor13 <owneruserbot@gmail.com>
2025-08-16 21:30:27 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -5,20 +5,20 @@ load_dotenv(".env")
 
 MAX_BOT = int(os.getenv("MAX_BOT", "200"))
 
-DEVS = list(map(int, os.getenv("DEVS", "7908003544").split()))
+DEVS = list(map(int, os.getenv("DEVS", "").split()))
 
-API_ID = int(os.getenv("API_ID", "23524337"))
+API_ID = int(os.getenv("API_ID", ""))
 
-API_HASH = os.getenv("API_HASH", "77a7094ef80a4296f5d4f54638e833fa")
+API_HASH = os.getenv("API_HASH", "")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8276692772:AAHksYZp8fyls08qztxoNLtHL5R9fokHYZI")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 
-OWNER_ID = int(os.getenv("OWNER_ID", "1714407109"))
+OWNER_ID = int(os.getenv("OWNER_ID", ""))
 
-BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002312835928").split()))
+BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "").split()))
 
-RMBG_API = os.getenv("RMBG_API", "a6qxsmMJ3CsNo7HyxuKGsP1o")
+RMBG_API = os.getenv("RMBG_API", "")
 
-MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://aortulsk:KxCX5EQdssavL4dm@cluster0.qsywpr2.mongodb.net/")
+MONGO_URL = os.getenv("MONGO_URL", "")
 
-LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-4753971361"))
+LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", ""))
