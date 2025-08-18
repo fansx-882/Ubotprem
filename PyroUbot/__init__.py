a19bf8f2075cf15cd935f00314ba07c4da873a63
fansx-882 <storefansx28@gmail.com>
2025-08-18 17:45:59 +0700

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -39,7 +39,7 @@ aiosession = ClientSession()
 
 class Bot(Client):
     def __init__(self, **kwargs):
-        super().__init__(**kwargs, device_model="ᴋɪɴɢᴘʀɪᴀɪɴᴅɪᴀ")
+        super().__init__(**kwargs, device_model="ғᴀɴsxʙᴏᴛ")
         
     def on_message(self, filters=None, group=-1):
         def decorator(func):
