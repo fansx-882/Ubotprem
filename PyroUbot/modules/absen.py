c3f759fc1a973c5aa6ae49ecac3de7f1ad1c914b
fansx-882 <storefansx28@gmail.com>
2025-08-25 17:10:03 +0700

    Update absen.py

--- PyroUbot/modules/absen.py
+++ PyroUbot/modules/absen.py
@@ -13,8 +13,8 @@ __MODULE__ = "Al Qur'an"
 __HELP__ = """
 <b>⦪ Bantuan Untuk Al Qur'an ⦫</b>
 
-<i>ᚗ Perintah : <code>{0}alquran 1 2</code>
-       untuk mencari ayat al quran</i>
+>ᚗ Perintah : <code>{0}alquran 1 2</code>
+       untuk mencari ayat al quran
  
 ᚗ Example : <code>{0}alquran 1 2</code>
 """
